69e79cbd9c5d6e40c0345b9181cc653d2b6736b4
Rheza Alfin <aRhez0903@outlook.com>
2021-06-14 13:48:54 +0700

    Update Seeder

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Laravel 8 + CoreUI + Livewire + Datatables
+
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat)](https://github.com/aRhez0903/Laravel-8-Livewire-CoreUI-Datatables/issues)
+
 ## About Laravel 8 + CoreUI + Livewire Datatables
 
 ## Whats Inside
@@ -5,8 +9,22 @@
 - Laravel Permisson (https://github.com/spatie/laravel-permission)
 - Livewire Datatables (https://github.com/mediconesystems/livewire-datatables)
 
-## Contributing
+Include simple Data Table with Livewire (CRUD).
 
+## Installation
+### First clone or download this repository
+After clone or download this repository, next step is install all dependency required by laravel and laravel-mix.
+
+```shell
+# install composer-dependency
+$ composer install
+# install npm package
+$ npm install
+# build dev 
+$ npm run dev
+```
+
+## Contributing
 Thank you for considering contributing to this repo!
 
 ## License
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +17,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //Seed Permissions
+        $bulks = ['index','create','view','edit','delete'];
+        $perms = ['access','user','permission','role'];
+        foreach($perms as $perm){
+            foreach($bulks as $bulk){
+                $data = ['name' => $perm.'-'.$bulk];
+                Permission::firstOrCreate($data);
+            }
+        }
+
+        //Seed Roles
+        $bulks = ['index','create','view','edit','delete'];
+        $roles = ['Developer','Super Administrator','Administrator','Staff'];
+        foreach($roles as $role){
+            $data = ['name' => $role];
+            Role::firstOrCreate($data);
+        }
+
+        $role = Role::findByName('Developer');
+        $permission = Permission::select('name')->get()->toArray();
+        //dd($permission);
+        $role->syncPermissions($permission);
+
+        $user = User::create([
+            'name' => 'Super Administrator',
+            'email' => 'super@admin.dev',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+        ]);
+
+        $user->syncRoles(['Developer','Super Administrator']);
+
         // \App\Models\User::factory(10)->create();
     }
 }
